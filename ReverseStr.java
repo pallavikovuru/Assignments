7c1844fc47afe1e341bbca1fc9ed6dc1385dc489
Pallavi <kovuru23@gmail.com>
2019-02-06 23:26:17 -0800

    My first commit 1

---
+++ ReverseStr.java
@@ -0,0 +1,36 @@
+package Assignments;
+
+public class ReverseStr {
+	public static void main(String[] args) {
+		String str = "pallavi";
+		String reverse = "";
+		int count = 0;
+		String vowels="";
+		String cons="";
+		//System.out.println(str.length());
+		//System.out.println(str.charAt(6));
+		for (int i = str.length()-1; i >= 0; i--) {
+			reverse = reverse + str.charAt(i);
+
+		}
+		System.out.println(reverse);
+
+		for (int j = 0; j < str.length(); j++) {
+			if (str.charAt(j) == 'a' || str.charAt(j) == 'e' || str.charAt(j) == 'i' || str.charAt(j) == 'o'
+					|| str.charAt(j) == 'u') {
+				count++;
+				vowels= vowels+str.charAt(j)+',' ;
+
+			}
+			else {
+				cons=cons+str.charAt(j)+',';
+				
+			}
+
+		}
+		System.out.println(count);
+		System.out.println(vowels);
+		System.out.println(cons);
+
+	}
+}

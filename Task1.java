4eecf12ef395eb4f8895250a7950e714a63a51b2
Pallavi <kovuru23@gmail.com>
2019-02-07 12:57:08 -0800

    second commit

---
+++ Task1.java
@@ -0,0 +1,20 @@
+package SeleniumBasics;
+
+public class Task1 {
+	public static void main(String args[]) {		
+		String s = "Mon";
+		int k = 500;
+		System.out.println(solution(s, k));
+	}
+	public static String solution(String S, int K) {
+		String [] days = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+		int index = -1;
+		for (int i = 0; i < days.length; i++) {
+			if (S.equals(days[i])) {
+				index = i;
+			}
+		}
+		int kIndex = (index + K) % 7;
+		return days[kIndex];
+    }
+}

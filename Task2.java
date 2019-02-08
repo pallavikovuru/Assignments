aa08c7104e3dfe178ac8a41c4b0c92c441f5647b
Pallavi <kovuru23@gmail.com>
2019-02-07 22:35:10 -0800

    added task2.java

---
+++ Task2.java
@@ -0,0 +1,26 @@
+package SeleniumBasics;
+
+public class Task2 {
+
+	public static void main(String args[]) {
+		int[] a = { -3, 0, 4, 1, 98, -3, -4 };
+		System.out.println(solution(a));
+	}
+
+	public static int solution(int[] A) {
+		int negativeCount = 0;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] < 0) {
+				negativeCount++;
+			} else if (A[i] == 0) {
+				return 0;
+			}
+
+		}
+		if (negativeCount % 2 == 0) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+}
